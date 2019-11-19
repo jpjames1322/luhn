1cfe33e93ead33a734964c617f9addb175388c55
jared-phillips <jp.james0927@gmail.com>
2019-11-19 01:18:11 +0000

    initial commit

---
+++ lib/luhn.rb
@@ -0,0 +1,17 @@
+module Luhn
+  def self.is_valid?(number)
+    number = number.to_s.chars.reverse
+    number = number.inject(:+)
+    sum = 0
+    number.each_with_index do |a, b|
+      if b.odd?
+        a = a * 2
+        if a > 9
+          a = a - 9
+        end
+      end
+      sum = sum + a
+    end 
+    return sum%10 == 0
+  end
+end
